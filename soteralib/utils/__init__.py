af841450684bc785eb0da6238199d65d369c9483
tterasaki <tomoki.terasaki@phys.s.u-tokyo.ac.jp>
2023-02-21 02:00:43 +0000

    add temperature converters

---
+++ soteralib/utils/__init__.py
@@ -0,0 +1,6 @@
+from Tconverter import (Tth2brightness, Trj2brightness, Tkcmb2brightness,
+                        brightness2Tth, brightness2Trj, brightness2Tkcmb,
+                        dBdT, dBdT_cmb,
+                        Tth2Trj, Tth2Tkcmb,
+                        Trj2Tth, Trj2Tkcmb,
+                        Tkcmb2Tth, Tkcmb2Trj)
