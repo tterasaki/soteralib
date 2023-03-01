7e94f8c5049a46d19ad041e75d52c66fa7479567
tterasaki <tomoki.terasaki@phys.s.u-tokyo.ac.jp>
2023-03-01 05:29:16 +0000

    fixed typo

--- soteralib/utils/__init__.py
+++ soteralib/utils/__init__.py
@@ -5,4 +5,4 @@ from .Tconverter import (Tth2brightness, Trj2brightness, Tkcmb2brightness,
                         Trj2Tth, Trj2Tkcmb,
                         Tkcmb2Tth, Tkcmb2Trj)
 
-from circular_stats import (circular_mean, circular_std)
+from .circular_stats import (circular_mean, circular_std)
